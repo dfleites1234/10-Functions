5cc57171632cda6c34ccb1466be346a7c17e9b62
Sparrow <dfleites1234@gmail.com>
2022-10-06 13:25:27 -0500

    The Call and Apply Methods

--- starter/script.js
+++ starter/script.js
@@ -98,7 +98,7 @@ document.body.addEventListener('click', high5); // Clicking the body logs the ðŸ
 
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
 
-*/
+
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Functions Returning Functions
@@ -119,3 +119,60 @@ greet('Hello')('Jonas');
 const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
 greetArr('Hi')('Jonas');
+
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// The Call and Apply Methods
+
+const lufthansa = {
+  airline: 'Kufthanse',
+  iataCode: 'LH',
+  bookings: [],
+  // book:function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Sarah Williams');
+
+// Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+// This is the same as the apply method. Except we use the spread ... operator to pull the data from flightData
+book.call(swiss, ...flightData);
